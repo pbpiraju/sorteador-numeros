ae8ddefb3c6bd6707e8602eac0c4859c20a51588
Thiago Interlichia <pbpiraju@hotmail.com>
2024-03-15 04:06:49 -0300

    Sorteador-de-Numeros

---
+++ app.js
@@ -0,0 +1,8 @@
+function sortear(params) {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+    console.log (`Quantidade${quantidade}`);
+    console.log (`de,${de}`);
+    console.log (`ate${ate}`);
+}
